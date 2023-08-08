58944683e9ce350fb213985cf171be8fc2e3a856
nikhitha-kv <141739836+nikhitha-kv@users.noreply.github.com>
2023-08-08 18:33:00 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,37 @@
+from calc import *
+
+print("MENU:")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice: ")
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+        except ZeroDivisionError:
+            print("no 0")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        next_calculation = input("Let's do next calculation? (y/n): ")
+        if next_calculation == "n":
+          break
+    else:
+        print("Invalid Input")
